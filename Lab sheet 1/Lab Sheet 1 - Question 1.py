735b3703149ebe3b521bc018c47fd43c5da50b0d
Gc0066 <gc_97@hotmail.co.uk>
2016-10-13 15:51:34 +0100

    Question 1 baisc task
    Made the solution more efficient.

--- Lab sheet 1/Lab Sheet 1 - Question 1.py
+++ Lab sheet 1/Lab Sheet 1 - Question 1.py
@@ -1,37 +1,45 @@
 from random import randint
+#decided to reprogram this question to make it more efficient. More efficient as
+#has one less list and does no longer have to continually randomly generate a number
+#until it has one that it had not generated before.
+def shuffle(usuableList, number, newList):
+    '''Function will shuffle a list, creating a new version of it and
+    removing items from the old as they are randomly placed into the new list.
+    Inputs are the list version of the input, an integer that will be a random
+    number after first call and ta list that is the return list.'''
+
+    #checks fro base case
+    if len(usuableList) == 1:
+        #if there is only one item left in the list, then put that last
+        #item in the new list and then return it so it can be output.
+        newList.append(usuableList[0])
+        return newList
+    else:
+        #generates random integer, 
+        number = randint(0, len(usuableList)-1)
+        #adds it to new list
+        newList.append(usuableList[number])
+        #removes the item that has been added to new list from the old list.
+        # So that it can no longer be pointed to during the random generation.
+        usuableList.remove(usuableList[number])
+        #calls the function again so the process can repeat till only one item is in the
+        #list.
+        return shuffle(usuableList, number, newList)
+    
+
+
+
 #inputs list
 inputList = input("Please enter a series of numbers")
 #splits up entered characters on the , to form a list
 usuableList = inputList.split(",")
-
 #initalises what will be our random number
 number = 0
 #initalises the new list
 newList = []
 #initalises random numbers generated list
 numberList = []
-#initalises boolean value used ot exit while loop
-boolValue = False
 
-#
-for i in usuableList:
-    #sets boolean value to false for every new number.
-    boolValue = False
-    #generates random number.
-    number = randint(0, len(usuableList)-1)
-    while boolValue == False:
-        #if the number generated has not been geenrated before
-        if number not in numberList:
-            #adds part of list in the indice specified by the random generation
-            newList.append(usuableList[number])
-            #to exit loop
-            boolValue = True
-            #adds number to the randomly generated list
-            numberList.append(number)
-        else:
-            #trys a new random number
-            number = randint(0, len(usuableList)-1)
-
-print(newList)
-
-#attempt with recrusion
+#calls the shuffle, returning a shuffled version of the list.
+print(shuffle(usuableList, number, newList))
+    
