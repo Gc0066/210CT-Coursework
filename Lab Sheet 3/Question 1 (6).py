d51957fbbcdfd1045d2fd9d3196bd4dc6f902591
Gc0066 <gc_97@hotmail.co.uk>
2016-10-23 18:46:04 +0100

    Question 6 and 7 done
    Big O added to both questions. Also made it so space was added after
    each word was inserted so that sentence came out correctly.

--- Lab Sheet 3/Question 1 (6).py
+++ Lab Sheet 3/Question 1 (6).py
@@ -2,21 +2,24 @@
 #due too having to pass the variables to the function. Rather than just use
 #them immediately.
 
-inputSentence= input("Please input a sentence you would like reversed")
+inputSentence= input("Please input a sentence you would like reversed")  #(1)
 
 #splits the inputted sentence into just the words
-splitSentence = inputSentence.split()
+splitSentence = inputSentence.split()        #(1)
 
 #print(splitSentence)
-reversedString = ""
+reversedString = ""          #(1)
 
 #finds the amount of words in the list. Takes away one as the list
 #is indexed from 0 so that loop therefore doesn't begin outside of list.
-i = len(splitSentence) -1
+i = len(splitSentence) -1          #(1)
 
 #from the end of the list to the beginning. Add the item at position i in the
 #list to the new string.
-while i >=0:
-    reversedString = reversedString + " " + splitSentence[i]
-    i = i -1
-print(reversedString)
+while i >=0:            #(n)
+    reversedString = reversedString + splitSentence[i] + " "        #(n)
+    i = i -1           #(n)
+print(reversedString)       #(1)
+
+#run time: 3n+5
+#big O: O(n)
