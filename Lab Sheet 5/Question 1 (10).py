25b9d4d358174dc6bb161f45d3b37d5536ce246f
gc0066 <gc_97@hotmail.co.uk>
2016-11-03 17:44:36 +0000

    Continuation of debugging
    More debugging

--- Lab Sheet 5/Question 1 (10).py
+++ Lab Sheet 5/Question 1 (10).py
@@ -1,4 +1,4 @@
-inputSeq = [1,4,3,2,5,6]
+inputSeq = [1,4,3,4,5,6,1]
 
 biggestSequence = 0
 subSequence = []
@@ -6,30 +6,40 @@ sameSize = False
 listOfStartingIndex = []
 number = 0
 i = 0
-
-while i != len(inputSeq)-1:
-    print(inputSeq[i], "this is input")
-    if inputSeq[i+1] >= inputSeq[i]:
+#print(len(inputSeq)-1)
+while i <= len(inputSeq)-1:
+    print(i, "this is input")
+    if i == 0:
+        number = 1
+    elif i == len(inputSeq)-1:
         number = number + 1
-        print(number)
-
     else:
-        if number > biggestSequence:
-            listOfStartingIndex.append(i-number)
-            biggestSequence = number
-            number = 0
-            print(biggestSequence, "big Seq")
-        elif number == biggestSequence:
-            sameSize = True
-            listOfStartingIndex.append(i-number)
-            number = 0
-            print("Why are you running")
+        if inputSeq[i+1] >= inputSeq[i]:
+
+            number = number + 1
+            #print(number)
+
+        else:
+            if number > biggestSequence:
+                #print(i)
+                listOfStartingIndex.append(i-number)
+                biggestSequence = number
+                number = 0
+                #print(biggestSequence, "big Seq")
+            elif number == biggestSequence:
+                sameSize = True
+                listOfStartingIndex.append(i-number)
+                number = 0
+                #print("Why are you running")
+            else:
+                number = 0
+                print("Is this run")
     i = i +1
 
 #think loop could be working correctly but next bit not
-
+print(listOfStartingIndex)
 if sameSize == True:
-    print("whya u run")
+    #print("whya u run")
     for n in listOfStartingIndex:
         subSequence.append(inputSeq[inputSeq[listOfStartingIndex[n]]:listOfStartingIndex[n]+number])
 else:
