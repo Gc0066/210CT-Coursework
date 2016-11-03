34d2a92be2bb611e9482d64fce0058189b0f4e9b
Gc0066 <gc_97@hotmail.co.uk>
2016-11-03 15:48:42 +0000

    Debugging Question 10
    Print statements added. Think output part is the problem.

--- Lab Sheet 5/Question 1 (10).py
+++ Lab Sheet 5/Question 1 (10).py
@@ -8,21 +8,32 @@ number = 0
 i = 0
 
 while i != len(inputSeq)-1:
+    print(inputSeq[i], "this is input")
     if inputSeq[i+1] >= inputSeq[i]:
         number = number + 1
+        print(number)
+
     else:
         if number > biggestSequence:
             listOfStartingIndex.append(i-number)
             biggestSequence = number
+            number = 0
+            print(biggestSequence, "big Seq")
         elif number == biggestSequence:
             sameSize = True
             listOfStartingIndex.append(i-number)
+            number = 0
+            print("Why are you running")
     i = i +1
 
+#think loop could be working correctly but next bit not
+
 if sameSize == True:
+    print("whya u run")
     for n in listOfStartingIndex:
-        subSequence.append(inputSeq[inputSequence[listOfStartingIndex[n]]:listOfStartingIndex[n]+number])
+        subSequence.append(inputSeq[inputSeq[listOfStartingIndex[n]]:listOfStartingIndex[n]+number])
 else:
-    subSequence.append(inputSeq[inputSequence[listOfStartingIndex[0]]:listOfStartingIndex[0]+number])
+    print("are")
+    subSequence.append(inputSeq[inputSeq[listOfStartingIndex[0]]:listOfStartingIndex[0]+number])
 
 print(subSequence)
