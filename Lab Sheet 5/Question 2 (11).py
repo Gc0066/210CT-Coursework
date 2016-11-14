410e42e3e7af37863def66d5889523bfe8589949
Gc0066 <gc_97@hotmail.co.uk>
2016-11-14 14:18:16 +0000

    Question 11
    Delete functon coded.

--- Lab Sheet 5/Question 2 (11).py
+++ Lab Sheet 5/Question 2 (11).py
@@ -5,17 +5,23 @@ class Node(object):
         self.next=None
         self.prev=None
 
+
  
-class List(object): 
+class List(object):
+    
     def __init__(self):
         #initalises list with a head and a tail but does not assign a value to either
         #as no nodes yet.
         self.head=None
         self.tail=None
        # self.previousNode = None
+        self.listOfNodes = []
+        self.i = 0
         
     def insert(self,n,x):
-        
+        self.listOfNodes.append(x)
+
+
         #Not actually perfect: how do we prepend to an existing list?
 
         #n is referencing none or it is referencing the head node
@@ -33,9 +39,9 @@ class List(object):
             #then makes the current node point to the node before it
             x.prev=n
             #htis if maybe stops the list?
-            print(x.next, "the next node to current one being initialsied")
-            print(n.next, "the next node for the one that was initalsied before the current one")
-            print(x.prev, "the previous node to the one currently being initialsied")
+            #print(x.next, "the next node to current one being initialsied")
+            #print(n.next, "the next node for the one that was initalsied before the current one")
+            #print(x.prev, "the previous node to the one currently being initialsied")
             
             
             if x.next!=None:
@@ -58,6 +64,7 @@ class List(object):
 
             
     def display(self):
+        #comment this
         values=[]
         n=self.head
         while n!=None:
@@ -65,38 +72,74 @@ class List(object):
             n=n.next
         print("List: ",",".join(values))
 
-    def delete(self,searchNode,deleteNode):
-        #slide 65
-        valueFound = False
-        while valueFound = False:
-            if searchNode.next == deleteNode.value:
-                then delete searchNode.next
-                valuefound = True
-        #this bit will replace the "then delete search.next" bit in the loop
-        if n != None:
-            n.prev.next = n.next
-        else:
-            l.head = n.next
-        if n.next != 0:
-            n.next.prev = n.prev
-        else:
-            l.tail = n.prev
+    def delete(self,deleteNode):
         
+        nodeFound = False
+        #sets the first node to be checked as the first node in the list
+        searchNode = self.head
+        #until node is found
+        while nodeFound == False:
+            
+            print(searchNode)
+            print(searchNode.value)
+
+            #if the current node is the desired node to be
+            #deleted.
+            #if searchNode.value == deleteNode:
+            if searchNode == deleteNode:
+
+                #if it is not the head
+                if searchNode.prev != None:
+                    #make the previous node point to
+                    #the node in front of the node being
+                    #deleted.
+                    searchNode.prev.next = searchNode.next
+                #make the node that will be the first node
+                #after deletion the head node.
+                else:
+                    l.head = searchNode.next
+
+                #if it is not the tail
+                if searchNode.next != None:
+                    #make the next node point to
+                    #the node previous to the node being
+                    #deleted.
+                    searchNode.next.prev = searchNode.prev
+                    
+                else:
+                    #make the node that will be the last node
+                    #after deletion the tail node.
+                    l.tail = searchNode.prev
+                    
+                searchNode.del
+                nodeFound = True
+                
+            #if at the end of the linked list and the
+            #current node is not the desired delete node.
+            elif searchNode.next == None:
+                return False
+
+            #otherwise go to next node
+            else:
+                searchNode = searchNode.next
+
+     
 ##            
          
 if __name__ == '__main__':
     l=List()
-    #calls list function and withn the parameters it creates the a node object and then passes that to the
+    #calls list function and withn the parameters it creates a node object and then passes that to the
     #insert function.
 
     #none given in first to make it the head of list.
     l.insert(None, Node(4))
     #after first node, .head is assigned to the first node thus l.head refernces the first node.
+    l.insert(l.head, Node(5))
     l.insert(l.head,Node(6))
     l.insert(l.head,Node(8))
     l.insert(l.head, Node(10))
-    #must have used the value somehow
-    #searches from head to tail of the linked list for value and then deletes the node that has that value
-    #could use the actual nodes name instead
-    l.delete(10)
+
+    #could either search for value.
+    #or could search by node using dictionary.
+    l.delete(l.listOfNodes[4])
     l.display()
